94cd7d2a03795fb9b51e09cce4f7c5794d088467
Ivan Sumenkov <sumenkov.ivan2007@gmail.com>
2026-02-18 18:49:21 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,3 @@
+f = open("C:/Users/Иван/OneDrive/Документы/sumenkov.ia/projects_2/task_2_0/output.txt", "w", encoding="utf-8")
+print("Суменков Иван Антонович\nГруппа: 4731204/50001\nИнститут: ИБСиБ", file=f)
+f.close()
